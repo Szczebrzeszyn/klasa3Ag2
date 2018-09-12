39be9a7b1fa69f405c332a5d8ad467f2671e19a6
lo1cgsan <lo1cgsan@gmail.com>
2018-09-12 09:40:53 +0200

    cpp, python powtórzenie podstaw

---
+++ cpp/funkcje.cpp
@@ -0,0 +1,39 @@
+/*
+ * szablon.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int sumuj(typ parametr, typ parametr)
+{
+    wynik = działania
+    return wynik
+}
+
+
+int main(int argc, char **argv)
+{
+    int a, b;
+    a = b = 0;
+    
+    cout << "Podaj 1. liczbę: ";
+    cin >> a;
+    cout << a;
+
+    cout << endl << "Podaj 2. liczbę: ";
+    cin >> b;
+    cout << b;
+
+    cout << endl << "Suma: " << a + b << endl;
+    cout << "Różnica: " << a - b << endl;
+    cout << "Iloczyn: " << a * b << endl;
+    cout << "Iloraz: " << a / b << endl;
+
+// DRY – Don't repeat yourself
+
+    return 0;
+}
+
