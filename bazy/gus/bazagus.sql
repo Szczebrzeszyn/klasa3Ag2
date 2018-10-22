40ba1f0f5dff348dc1cb2a658a8da77db36daf64
lo1cgsan <lo1cgsan@gmail.com>
2018-10-22 14:21:31 +0200

    skrypt tworzący bazę i dodający dane, kwerenda wybierająca

--- bazy/gus/bazagus.sql
+++ bazy/gus/bazagus.sql
@@ -1,15 +1,16 @@
 -- bazagus.sql
 DROP TABLE IF EXISTS miasta;
 DROP TABLE IF EXISTS dane_demograficzne;
+DROP TABLE IF EXISTS powierzchnie;
 
 CREATE TABLE miasta (
-    id_miasta INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_miasta INTEGER PRIMARY KEY,
     nazwa_miasta TEXT(30),
     wojewodztwo TEXT(30)
 );
 
 CREATE TABLE dane_demograficzne (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER,
     liczba_mieszkancow INTEGER,
     liczba_kobiet INTEGER,
     grupa_wiekowa TEXT(15),
@@ -18,15 +19,17 @@ CREATE TABLE dane_demograficzne (
     FOREIGN KEY (id_miasta) REFERENCES miasta(id_miasta)
 );
 
-DROP TABLE IF EXISTS powierzchnie;
 CREATE TABLE powierzchnie (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER,
     powierzchnia_miasta DECIMAL,
     powierzchnie_zielone DECIMAL,
     data_akt DATE,
     id_miasta INTEGER,
     FOREIGN KEY (id_miasta) REFERENCES miasta(id_miasta)
 );
+
+-- INSERT INTO miasta VALUES(NULL, 'Warszawa', 'Mazowieckie');
+
 -- sqlite3 baza.db < bazagus.sql
 -- sqlite3 baza.db
 -- sqlite>.table – wyświetla tabele
